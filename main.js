22d6f2f0ba5c3904e4e64930661e03e1003b965e
Sanchika123 <79789552+Sanchika123@users.noreply.github.com>
2021-10-12 22:33:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 Status="";
+objects="";
 
 function preload(){
-    img= loadImage('https://previews.123rf.com/images/serezniy/serezniy1110/serezniy111000083/10754080-fresh-vegetables-in-basket-on-wooden-table-on-green-background.jpg');
+    img= loadImage('https://media.istockphoto.com/photos/healthy-fresh-rainbow-colored-fruits-and-vegetables-background-picture-id1208790371?k=20&m=1208790371&s=612x612&w=0&h=6BngNrl8TColGkvSGJUKFKIM5bv31Nc8MvQhmmC2LlM=')
 }
 
 function setup(){
-    canvas=createCanvas(640, 420);
+    canvas=createCanvas(380, 380);
     canvas.center();
     objectDetector= ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById('status').innerHTML= 'status: detecting objects';
@@ -26,5 +27,21 @@ function gotresult(error, results){
     }
 }
 function draw(){
-    image(img, 0, 0, 640, 420);
+    image(img, 0, 0, 380, 380);
+    if(Status != ""){
+        objectDetector.detect(img,gotresult);
+        r= random(255);
+        g= random(255);
+        b= random(255);
+        for(i=0; i<objects.length; i++){
+            document.getElementById('status').innerHTML= 'status: objects detected';
+            document.getElementById('object_number').innerHTML= 'Number of objects detected: '+objects.length;
+            percent= floor(objects[i].confidence*100);
+            fill(r, g, b);
+            text(objects[i].label +" "+percent+"%", objects[i].x+15, objects[i].y+15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
         }
+    }
+}
